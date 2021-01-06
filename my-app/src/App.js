aa2fa7ad8c8ca855174f18934f32d4ab8282e2c9
Sherzodbek <sherzodbekoffical@gmail.com>
2021-01-06 13:58:21 +0500

    lorem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div>
       <h2>hello world</h2>
       <p>world</p>
+      <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </h4>
     </div>
   );
 }
