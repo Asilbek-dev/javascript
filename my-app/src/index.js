a2a56b0d3c2361e26b2c0677bf7ba7e22e44a5b4
Asilbek <ergashevasilbek41@gmail.com>
2021-01-06 20:26:41 +0500

    push

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.css';
-import reportWebVitals from './reportWebVitals';
-// import "bootstrap/dist/css/bootstrap.css";
+import App from './App';
 import "antd/dist/antd.css";
-import "../src/components/main.css";
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import './components/main.css';
+
 
 ReactDOM.render(
-  <reportWebVitals>
-    <App />
-  </reportWebVitals>,
-  document.getElementById('root')
+  <Provider store={store}>
+     <BrowserRouter>
+         <App/>
+     </BrowserRouter>
+  </Provider>,
+document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
